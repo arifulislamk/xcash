6683243be2aae93d4d0acc36f7af9a86ac915bff
arifulislamk <ariful18374@gmail.com>
2024-07-16 08:15:25 +0600

    mongodb and login design

--- xcash-client/src/Layout/Main.jsx
+++ xcash-client/src/Layout/Main.jsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 const Main = () => {
   return (
     <div>
-      <h2 className=" text-5xl text-left font-bold">Xcash</h2>
       <Outlet />
     </div>
   );
--- xcash-client/src/pages/Login.jsx
+++ xcash-client/src/pages/Login.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
   return (
     <div>
       <div className="hero bg-base-200 min-h-screen">
         <div className="hero-content flex-col">
           <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
+            <h1 className="text-3xl font-bold text-center">Login</h1>
+            <p>Signup your Xcash account?</p>
           </div>
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
             <form className="card-body">
@@ -33,6 +36,7 @@ const Login = () => {
               <div className="form-control mt-6">
                 <button className="btn btn-primary">Login</button>
               </div>
+              <p>Are you new? <Link className=" text-blue-500" to="/registration">Register</Link></p>
             </form>
           </div>
         </div>
